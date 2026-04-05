fbe6b043add0e65e43a2ce2181a22911467ea0cd
Presley He <phe4292@gmail.com>
2026-04-04 22:08:02 -0400

    DOM review in progress; progress made: Accessing elemnts using document.querryselector(), creating new elements, and changing text.

--- eX/script.js
+++ eX/script.js
@@ -3,16 +3,29 @@ for(let i = 0; i < 10; i++) {
     console.log("Testing #" + n)
     n += 1
 }
-function hi(message) {
-    console.log(message)
-    alert(message)
-}
-let message = prompt("What message would you like to print?: ")
-hi(message)
+// function hi(message) {
+//     console.log(message)
+//     alert(message)
+// }
+// let message = prompt("What message would you like to print?: ")
+// hi(message)
 
 // Dictionaries
-let value = 1
+let value = prompt("What should be the value?: ")
 let dict = {
     "key": value
 }
 console.log(dict["key"])
+
+// DOM
+header = document.querySelector(".header")
+div = document.querySelector(".div1")
+function addText() {
+    let text = prompt("What would you like the container to say?: ")
+    let new_text = document.createElement("p")
+    new_text.textContent = text
+    div.append(new_text)
+}
+addText()
+// header.textContent = "new Text"
+
